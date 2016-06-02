2440ad2ea815780f038095bf6bebb212797603cc
lundgren84 <lundgren84@hotmail.se>
2016-06-02 11:34:42 +0200

    sss

---
+++ Html_inlämningar/Html5_css_Bootstrap_inlämning4/MoreScript/MaldiveGames.js
@@ -0,0 +1,8 @@
+﻿
+window.onload = function () {
+    var c = document.getElementById("backcontainer");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("tankback");
+    ctx.drawImage(img, 0, 0,img.width,img.height,0,0,
+        c.width,c.height);
+}
